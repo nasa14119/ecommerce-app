8eb2f426cc55ae1685cd589c8204db5e81497302
nasa14119 <nico.amaya0205@gmail.com>
2024-07-09 21:46:04 -0600

    adding the index page

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,7 @@ module.exports = {
         ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
+        brown: "#241915", 
         primary: {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
