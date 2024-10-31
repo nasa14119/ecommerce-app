25061b06a88c21ff73f8e249999561be3fa67584
nasa14119 <nico.amaya0205@gmail.com>
2024-10-31 17:40:00 -0600

    feat(LangBtn) adding btn home

--- src/components/home/LangBtn/index.tsx
+++ src/components/home/LangBtn/index.tsx
@@ -20,23 +20,20 @@ export function LangBtn(props: any) {
     </>
   );
 }
+//TODO make the nav lang btn for desktop, and add the change language funcionality
 export function LangBtnDesktop() {
-  const [state, setValue] = useToogle();
   return (
-    <>
-      <button className={`font-bold`} onClick={() => setValue()}>
+    <div className={`${styles.lang_desktop} group cursor-default`}>
+      <span className="font-bold absolute inset-0 grid place-content-center group-hover:-translate-x-full">
         lang
-      </button>
+      </span>
       <span
-        className={`pl-2 flex gap-x-2 text-sm z-50 
-          ${state === null && "hidden"}
-          ${state ? `${styles.btn_open}` : `${styles.btn_close}`}
-        `}
+        className={`text-sm absolute inset-0 grid place-content-center translate-x-full group-hover:translate-x-0 ${styles.lang_opt_desktop}`}
       >
         <button>ES</button>
         <span>/</span>
         <button>EN</button>
       </span>
-    </>
+    </div>
   );
 }
